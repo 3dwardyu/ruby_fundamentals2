faa87809cfe3b990ca1f4c72e52536b22f7dbf8b
Edward Yu <3dward.yu@gmail.com>
2016-08-09 14:46:56 -0400

    Add user input

--- exercise5.rb
+++ exercise5.rb
@@ -1,7 +1,8 @@
 def convert(f)
-  f.to_i
   celsius = (f - 32) * 5/9
   puts "#{f} Fahrenheit is #{celsius} Celsius"
 end
 
-convert(75)
+puts "Enter temperature to convert"
+temp = gets.chomp.to_i
+convert(temp)
