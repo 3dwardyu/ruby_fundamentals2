2d795b7368abafda6d0513075a9352327e4afcf6
Edward Yu <3dward.yu@gmail.com>
2016-08-09 15:58:31 -0400

    add asterisk in front of items

--- exercise6.rb
+++ exercise6.rb
@@ -1,8 +1,9 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 def list(g)
-  g.each { |i| puts "#{i}"}
+  g.each { |i| puts "*#{i}"}
   puts "You have #{g.length} items"
+
 end
 
 list(grocery_list)
