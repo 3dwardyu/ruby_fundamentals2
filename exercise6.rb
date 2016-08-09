7a242e91c30ee8ce65d27f4faa3edb1a20204695
Edward Yu <3dward.yu@gmail.com>
2016-08-09 16:24:42 -0400

    Sort list in alpha order

--- exercise6.rb
+++ exercise6.rb
@@ -3,11 +3,23 @@ grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 def list(g)
   g.each { |i| puts "*#{i}"}
   puts "You have #{g.length} items"
-  puts g.include?("bananas") ? "You don\'t need bananas today" : "You need to pick up bananas"
 end
 
+#Displays the list the 1st time
 list(grocery_list)
 
+#Adds rice to the list
 grocery_list << "rice"
 
+#Displays the new list with rice
+list(grocery_list)
+
+#Checks list for bananas
+puts grocery_list.include?("bananas") ? "You don\'t need bananas today" : "You need to pick up bananas"
+
+#Displays the 2nd item in the list
+puts grocery_list[1]
+
+#Sorts the list in alphabetical order
+grocery_list.sort
 list(grocery_list)
