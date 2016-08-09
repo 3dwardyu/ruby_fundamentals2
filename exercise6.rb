0dce13c0a9bd90ea4565fa70eb74da32efa23d6d
Edward Yu <3dward.yu@gmail.com>
2016-08-09 16:14:26 -0400

    add check for bananas

--- exercise6.rb
+++ exercise6.rb
@@ -3,7 +3,7 @@ grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 def list(g)
   g.each { |i| puts "*#{i}"}
   puts "You have #{g.length} items"
-
+  puts g.include?("bananas") ? "You don\'t need bananas today" : "You need to pick up bananas"
 end
 
 list(grocery_list)
