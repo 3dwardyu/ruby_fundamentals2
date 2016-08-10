7a6262c809d7a67cae6ab40fe428a8297070da5b
Edward Yu <3dward.yu@gmail.com>
2016-08-10 13:40:35 -0400

    add line to display cohort names and added comments

--- exercise7.rb
+++ exercise7.rb
@@ -1,11 +1,20 @@
+# create initial hash for Bitmaker cohorts
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
 
+# method to display cohort name and number of students
 def display(list)
   list.each {|a, b| puts "#{a}: #{b} students"}
 end
 
+# display cohort and number of students
 display(students)
+
+# add a 4th cohort
+students[:cohort4] = 43
+
+# display cohort names
+puts students.keys
